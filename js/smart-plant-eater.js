32190c23dba9c1360dff3f85fe2757633d9909da
seanng <shonum@gmail.com>
2016-03-01 10:27:44 +0800

    done

--- js/smart-plant-eater.js
+++ js/smart-plant-eater.js
@@ -1,9 +1,42 @@
-function SmartPlantEater() {};
-
-SmartPlantEater.prototype.act = function(view) {  
-  // This is just a dummy behavior
-  return {
-    type: "move", 
-    direction: "e"
-  };
+function SmartPlantEater() {
+  this.energy = 20;
+  this.hungry = false;
 };
+
+SmartPlantEater.prototype.act = function(view) {
+  this.hungerCheck();
+  var space = view.find(" ");
+  var plant = view.find("*");
+  var found = view.findAll();
+
+  if (this.hungry && plant){
+    return {type: "eat", direction: plant};
+  }
+
+  // if (this.energy===20 && space) {
+  //   return {type: "reproduce", direction: space};
+  // }
+
+  if (this.energy>24 && space){
+    return {type:"reproduce", direction: space};
+  }
+
+  if (space){
+    return {type: "move", direction: space};
+  }
+
+  if (!this.hungry && found.length==8){
+    this.hungry = true;
+    this.energy = 5
+  }
+};
+
+SmartPlantEater.prototype.hungerCheck = function(view) {
+
+  if (this.energy <= 20) {
+    this.hungry = true;
+  }
+  if (this.energy >= 30) {
+    this.hungry = false;
+  }
+}
